aa394e27d6e5a32e1d8da536705242fd3abaed9a
peter <magyarpeter222@gmail.com>
2026-02-13 22:41:01 +0100

    finish tokenizer

---
+++ instruction_following/special_tokens.py
@@ -0,0 +1,11 @@
+special_tokens: dict[str, str] = {
+    "end_of_text": "<|endoftext|>",
+    "end_of_turn": "<|endofturn|>",
+    "bos": "<|bos|>",
+    "eos": "<|eos|>",
+    "user": "<|user|>",
+    "assistant": "<|assistant|>",
+    "system": "<|system|>",
+    "pad": "<|pad|>",
+    "unk": "<|unk|>"
+}
---
+++ instruction_following/tokenizer_datasets.py
@@ -0,0 +1,68 @@
+import os
+from typing import Iterator
+
+from datasets import load_dataset
+from huggingface_hub import login
+
+hf_token = os.getenv("HF_TOKEN")
+
+login(hf_token)
+
+
+def tokenizer_lite_dataset():
+    total_length = 0
+
+    # dataset
+    no_robots_ds = load_dataset("HuggingFaceH4/no_robots", streaming=True, split="test").select_columns(
+        ["messages"])
+    total_length += no_robots_ds.dataset_size
+
+    # iterator
+    def iterator() -> Iterator[str]:
+        for row in no_robots_ds:
+            for msg in row["messages"]:
+                yield msg["content"]
+
+    return iterator, total_length
+
+
+def tokenizer_real_dataset():
+    total_length = 0
+
+    # no robots
+    no_robots_ds = load_dataset("HuggingFaceH4/no_robots", streaming=True, split="test").select_columns(["messages"])
+    total_length += no_robots_ds.dataset_size
+
+    # wiki
+    wiki_ds = load_dataset("rahular/simple-wikipedia", streaming=True, split="train").select_columns(["text"])
+    reduced_count = int(wiki_ds.dataset_size / 100)
+    wiki_ds = wiki_ds.take(reduced_count)
+    total_length += reduced_count
+
+    # tiny stories
+    tiny_stories_ds = load_dataset("roneneldan/TinyStories", streaming=True, split="validation").select_columns(
+        ["text"])
+    total_length += tiny_stories_ds.dataset_size
+
+    # tiny textbooks
+    tiny_textbooks_ds = load_dataset("nampdn-ai/tiny-textbooks", streaming=True, split="test").select_columns(
+        ["textbook"])
+    total_length += tiny_textbooks_ds.dataset_size
+
+    # iterator
+    def iterator() -> Iterator[str]:
+        for row in no_robots_ds:
+            for msg in row["messages"]:
+                yield msg["content"]
+        print("no robots completed")
+        for row in wiki_ds:
+            yield row["text"]
+        print("wiki completed")
+        for row in tiny_stories_ds:
+            yield row["text"]
+        print("tiny stories completed")
+        for row in tiny_textbooks_ds:
+            yield row["textbook"]
+        print("tiny textbooks completed")
+
+    return iterator, total_length
