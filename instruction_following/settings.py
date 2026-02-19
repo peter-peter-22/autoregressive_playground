bbfe5d2922ffbeb13826ce93a8a56f6eab5bcf3a
peter <magyarpeter222@gmail.com>
2026-02-19 12:19:21 +0100

    more adjustments

--- instruction_following/settings.py
+++ instruction_following/settings.py
@@ -1,10 +1,9 @@
 class ModelSettings:
     vocabulary_size = 24_000
-    embedding_size = 500
-    embedding_dropout = 0.1
+    embedding_size = 384
     max_context_length = 512
     ff_size_multiplier = 4
     transformer_blocks = 12
-    attention_heads = 10
+    attention_heads = 12
     dropout = 0.1
     bias = True
